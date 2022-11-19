e29f0452bdbb0c39363768067f75722c748aa27f
MikeChara <mikejchara@gmail.com>
2022-11-19 20:11:09 +0000

    deleted some junk and the app still runs!

---
+++ revision-website/src/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;

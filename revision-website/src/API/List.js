d42e0566115b9bc507c45eaeb52936319c92cdff
MikeChara <mikejchara@gmail.com>
2022-11-20 18:59:37 +0000

    mvp pull of memes works

--- revision-website/src/API/List.js
+++ revision-website/src/API/List.js
@@ -24,14 +24,19 @@ export function Gallery() {
       <h2>
         <i>{meme?.name}</i>
       </h2>
-      <h3>
-        ({index + 1}) of {meme?.length}
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} showURL
-      </button>
-      {showMore && <p>{meme?.url}</p>}
+      <h3>({index + 1}) of 100</h3>
       <img src={meme?.url} alt={meme?.id} height="500px" />
+      <br></br>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show additional details"}
+      </button>
+      {showMore && (
+        <p>
+          url:{meme?.url}
+          <br></br>
+          usual text box count:{meme?.box_count}
+        </p>
+      )}
     </>
   );
 }
--- revision-website/src/pages/fetchandcreate.js
+++ revision-website/src/pages/fetchandcreate.js
@@ -1,18 +1,14 @@
-import React from "react";
-import InputButton from "../API/Inputs";
+import React, { useEffect } from "react";
 import fetchMemes from "../API/index";
 import { Gallery } from "../API/List";
 
-export function MemeFetch() {
-  console.log("fired");
-  fetchMemes();
-}
-
 const FetchAndCreate = () => {
+  useEffect(() => {
+    fetchMemes();
+  }, []);
   return (
     <div>
       <h1>Pick a popular meme and edit it!</h1>
-      <InputButton onClick={MemeFetch} />
       <Gallery />
     </div>
   );
